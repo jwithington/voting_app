1fb34b2a1202c0e3fa1d01299a14486f60930009
Jim Withington <jamesrwithington@gmail.com>
2019-07-09 20:46:09 -0700

    add function with constructor for upvoting and log to console

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 class ProductList extends React.Component {
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted.');
+  }
+  
   render() {
     const products = Seed.products.sort((a, b) => (
       b.votes - a.votes
@@ -16,6 +20,7 @@ class ProductList extends React.Component {
           votes={product.votes}
           submitterAvatarUrl={product.submitterAvatarUrl}
           productImageUrl={product.productImageUrl}
+          onVote={this.handleProductUpVote} // this passes the handleProductUpVote as a prop, so we can access it via this.props.onVote
       />
     );
     return (
@@ -27,6 +32,16 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
@@ -35,7 +50,7 @@ class Product extends React.Component {
         </div>
         <div className="middle aligned content">
           <div className="header">
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className="large caret up icon" />
             </a>
             {this.props.votes}
