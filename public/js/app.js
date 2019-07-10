79f81399b6b7abebb4fcc273f88711eeaaf27ea5
Jim Withington <jamesrwithington@gmail.com>
2019-07-09 22:10:54 -0700

    070919 add object assign and other methods and functions to use upvotes for sorting

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,42 @@
 class ProductList extends React.Component {
+
+  constructor(props) {
+    super(props); // this is just convention b/c we aren't actually using it
+
+    this.state = {
+      products: [], // initializing "empty" state is good practice
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this); // because we are using this for setState, we have to bind it to the component
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product) => { // traverse over the entire product array
+      if (product.id === productId) { // if upvoted, create new object and overwrite votes property
+        return Object.assign({}, product, {
+          votes: product.votes +1,
+        });
+      } else {
+        return product; // if not upvoted, just keep it as is
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
   
   render() {
-    const products = Seed.products.sort((a, b) => (
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
       // if return is less than 0, a comes first
       // if return is greater than 0, b comes first
       // if return is equal to 0, leave order unchanged
     ));
-    const productComponents = Seed.products.map((product) =>
+    const productComponents = products.map((product) =>
       <Product
           key ={'product-' + product.id}
           id={product.id}
